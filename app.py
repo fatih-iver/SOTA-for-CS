3166ebeed07eac16d5fad85664c72f65e54c9f5f
fatih-iver <fatih.iver@boun.edu.tr>
2019-05-09 12:25:07 +0300

    add admin control functionality

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+isAdmin = False
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def welcome():
+    return render_template('welcome.html')
+
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+def set_admin(true_or_false):
+    global isAdmin
+    isAdmin = true_or_false
+
+
+@app.route("/as_admin/", methods=['POST'])
+def as_admin():
+    set_admin(True)
+    return redirect(url_for('index'))
+
+
+@app.route("/as_user/", methods=['POST'])
+def as_user():
+    set_admin(False)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
