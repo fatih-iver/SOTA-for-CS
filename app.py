33147bc749c83d0992897346a615e5099545ccf0
fatih-iver <fatih.iver@boun.edu.tr>
2019-05-09 14:52:15 +0300

    create db scheme and db itself

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, g
+import sqlite3
 
 app = Flask(__name__)
 
 isAdmin = False
+
+DATABASE = 'sota.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 @app.route('/')
---
+++ initialize_db.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+conn = sqlite3.connect('sota.db')
+print("The database is opened successfully");
+
+conn.execute("""
+            CREATE TABLE IF NOT EXISTS authors (
+            author_id INTEGER PRIMARY KEY, 
+            author_name TEXT NOT NULL,
+            author_surname TEXT NOT NULL,
+            UNIQUE(author_name, author_surname) );
+            """)
+
+conn.execute("""
+            CREATE TABLE IF NOT EXISTS topics (
+            topic_id INTEGER PRIMARY KEY,
+            topic_name TEXT UNIQUE,
+            sota_result INTEGER UNIQUE );
+            """)
+
+conn.execute("""
+            CREATE TABLE IF NOT EXISTS papers (
+            paper_id INTEGER PRIMARY KEY,
+            title TEXT UNIQUE,
+            abstract TEXT,
+            result INTEGER );
+            """)
+
+conn.execute("""
+            CREATE TABLE IF NOT EXISTS paper_authors (
+            paper_id INTEGER,
+            author_id INTEGER,
+            PRIMARY KEY (paper_id, author_id),
+            FOREIGN KEY (paper_id) REFERENCES papers (paper_id)
+            ON DELETE CASCADE ON UPDATE NO ACTION,
+            FOREIGN KEY (author_id) REFERENCES authors (author_id) 
+            ON DELETE CASCADE ON UPDATE NO ACTION );
+            """)
+
+conn.execute("""
+            CREATE TABLE IF NOT EXISTS paper_topics (
+            paper_id INTEGER,
+            topic_id INTEGER,
+            PRIMARY KEY (paper_id, topic_id),
+            FOREIGN KEY (paper_id) REFERENCES papers (paper_id)
+            ON DELETE CASCADE ON UPDATE NO ACTION,
+            FOREIGN KEY (topic_id) REFERENCES topics (topic_id) 
+            ON DELETE CASCADE ON UPDATE NO ACTION );
+            """)
+
+print("Tables are created successfully");
+
+conn.close()
